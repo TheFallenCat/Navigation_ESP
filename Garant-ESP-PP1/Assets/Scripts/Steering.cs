314905e172efb276ab1f938cacb926966db39e88
TheFallenCat <mathgarant@gmail.com>
2025-02-07 14:53:33 -0500

    feat: SailingModel | update: SailSpeed

--- Garant-ESP-PP1/Assets/Scripts/Steering.cs
+++ Garant-ESP-PP1/Assets/Scripts/Steering.cs
@@ -61,26 +61,4 @@ public class Steering : MonoBehaviour
         //Camera.transform.position = Vector3.SmoothDamp(Camera.transform.position, transform.position + transform.forward * -8f + transform.up * 2f, ref CamVel, 0.05f);
     }
 
-    void ApplyForceToReachVelocity(Rigidbody rigidbody, Vector3 velocity, float force = 1, ForceMode mode = ForceMode.Force)
-    {
-        if (force == 0 || velocity.magnitude == 0)
-            return;
-
-        velocity = velocity + velocity.normalized * 0.2f * rigidbody.drag;
-
-        //force = 1 => need 1 s to reach velocity (if mass is 1) => force can be max 1 / Time.fixedDeltaTime
-        force = Mathf.Clamp(force, -rigidbody.mass / Time.fixedDeltaTime, rigidbody.mass / Time.fixedDeltaTime);
-
-        //dot product is a projection from rhs to lhs with a length of result / lhs.magnitude https://www.youtube.com/watch?v=h0NJK4mEIJU
-        if (rigidbody.velocity.magnitude == 0)
-        {
-            rigidbody.AddForce(velocity * force, mode);
-        }
-        else
-        {
-            var velocityProjectedToTarget = (velocity.normalized * Vector3.Dot(velocity, rigidbody.velocity) / velocity.magnitude);
-            rigidbody.AddForce((velocity - velocityProjectedToTarget) * force, mode);
-        }
-    }
-
 }
